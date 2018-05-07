fb36e08b86b55d31aab36dd9d246ed20bc7004e9
IanWernecke <IanWernecke@protonmail.com>
2018-05-07 16:25:09 -0400

    Lots of updates to mining operations.

--- build.py
+++ build.py
@@ -1,21 +1,47 @@
 #!python3
 
+
 import os
 import sys
 
 
+# define some global variables
 builder_py = os.path.abspath(__file__)
 base_dir = os.path.dirname(builder_py)
-ignore = [
+setup_ignore = [
     builder_py,
     os.path.join(base_dir, 'README.md')
 ]
 
+# github information
 github_user = 'IanWernecke'
 github_repo = 'ccturtle'
 github_branch = 'master'
 
+# the location to save the files on the pc
 pc_root = 'turtle'
+pc_setup = 'setup'
+
+# protected turtle directories
+turtle_sanitize = 'sanitize'
+turtle_ignore = [
+    'disk',
+    'openp',
+    'rom'
+]
+
+
+def lines_to_file(file_lines, file_name):
+    """
+    Write the lines to the file as the content of that file and return.
+
+    :return: the length of the files written
+    """
+    output = '\n'.join(file_lines)
+    with open(file_name, 'wb') as f:
+        f.write(output.encode('utf-8'))
+    return len(file_lines)
+
 
 def main():
     """
@@ -26,6 +52,7 @@ def main():
     lines = ['shell.run("rm {}")'.format(pc_root)]
     for walked_dir, sub_dirs, sub_files in os.walk(base_dir):
 
+        # skip git files and information
         if '.git' in walked_dir:
             continue
 
@@ -36,10 +63,10 @@ def main():
         # walk each file in the directory
         for sub_file in sub_files:
 
-            abs_file = os.path.join(rel_dir, sub_file)
+            abs_file = os.path.join(walked_dir, sub_file)
 
             # ignore garbage files
-            if abs_file in ignore:
+            if abs_file in setup_ignore:
                 continue
 
             # find the relative path to the directory
@@ -55,9 +82,18 @@ def main():
             ))
 
     # write the new setup file
-    output = '\n'.join(lines)
-    with open('setup', 'wb') as f:
-        f.write(output.encode('utf-8'))
+    lines_to_file(lines, 'setup')
+
+    # write the new cleaner file
+    lines = []
+    for f in os.listdir(base_dir):
+        if f in turtle_ignore:
+            continue
+        if '.git' == f:
+            continue
+        lines.append('rm /{}'.format(f))
+
+    lines_to_file(lines, 'sanitize')
 
     return 0
 
