2cc2c642fc40498b21f64ed380992b01a5d50ec9
IanWernecke <IanWernecke@protonmail.com>
2020-05-02 22:52:34 -0400

    Fixing new matching methodin the up file

--- mine/vein/up.lua
+++ mine/vein/up.lua
@@ -1,4 +1,8 @@
-local data = turtle.inspectUp()
+local success, data = turtle.inspectUp()
+if not success then
+  error("No block found.")
+end
+
 local count = mine.vein({name=data.name})
 
 print("Mined " .. tostring(count) .. " " .. data.name)
