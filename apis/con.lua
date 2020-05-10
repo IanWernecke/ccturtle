5e44a16d90168ce73edc9756c8baf371aab6011f
IanWernecke <IanWernecke@protonmail.com>
2020-05-09 22:09:12 -0400

    Adding recipes for buildcraft

--- apis/con.lua
+++ apis/con.lua
@@ -4,7 +4,9 @@ DIG_INTERVAL = 0.5
 
 -- mod prefixes for block and item details
 MOD_APPLIED_ENERGISTICS = "appliedenergistics2"
+MOD_BUILDCRAFT_BUILDERS = "buildcraftbuilders"
 MOD_CHISEL = "chisel"
+MOD_ENERGY_CONVERTERS = 'energyconverters'
 MOD_INDUSTRIAL_CRAFT = "ic2"
 MOD_MINECRAFT = "minecraft"
 MOD_NETHER_ORES = "NetherOres"
@@ -40,16 +42,27 @@ ITEM_GLASS = {name=MOD_MINECRAFT .. ":glass"}
 ITEM_GOLD_INGOT = {name=MOD_MINECRAFT .. ":gold_ingot"}
 ITEM_IRON_INGOT = {name=MOD_MINECRAFT .. ":iron_ingot"}
 ITEM_LOG = {name=MOD_MINECRAFT .. ":log"}
+ITEM_PISTON = {name=MOD_MINECRAFT .. ":piston"}
 ITEM_PLANKS = {name=MOD_MINECRAFT .. ":planks"}
 ITEM_REDSTONE = {name=MOD_MINECRAFT .. ":redstone"}
 ITEM_STICK = {name=MOD_MINECRAFT .. ":stick"}
+ITEM_STONE = {name=MOD_MINECRAFT .. ":stone"}
 
 
--- items: buildcraft
--- ITEM_QUARRY
+-- items: buildcraft/buildcraftbuilders
+ITEM_QUARRY = {name=MOD_BUILDCRAFT_BUILDERS .. ":quarry"} -- damage=0
+-- ITEM_STIRLING_ENGINE = {name=}
+
+
+-- items: energy converters
+ITEM_ENERGY_BRIDGE = {name=MOD_ENERGY_CONVERTERS .. ":energy_bridge"} -- damage=0
+ITEM_ENERGY_CONSUMER_EU_LV = {name=MOD_ENERGY_CONVERTERS .. ":energy_consumer_eu", damage=0} -- damage=4
+ITEM_ENERGY_PRODUCER_EU_LV {name=MOD_ENERGY_CONVERTERS .. ":energy_producer_eu", damage=0} -- guess
+ITEM_ENERGY_PRODUCER_MJ = {name=MOD_ENERGY_CONVERTERS .. ":energy_producer_mj", damage=0}
 
 
 -- items: industrial craft items
+ITEM_BASIC_MACHINE_CASING = {name=MOD_INDUSTRIAL_CRAFT .. ":resource", damage=12}
 ITEM_BATBOX = {name=MOD_INDUSTRIAL_CRAFT .. ":te", damage=72}
 ITEM_BATTERY = {name=MOD_INDUSTRIAL_CRAFT .. ":re_battery"}
 ITEM_CIRCUIT = {name=MOD_INDUSTRIAL_CRAFT .. ":crafting", damage=1}
@@ -77,9 +90,11 @@ ITEM_GOLD_GEAR = {name=MOD_THERMAL_FOUNDATION .. ":material", damage=25}
 ITEM_IRON_GEAR = {name=MOD_THERMAL_FOUNDATION .. ":material", damage=24}
 ITEM_IRON_PLATE = {name=MOD_THERMAL_FOUNDATION .. ":material", damage=32}
 ITEM_PULVERIZED_COAL={name=MOD_THERMAL_FOUNDATION .. ":material", damage=768}
+ITEM_STONE_GEAR = {name=MOD_THERMAL_FOUNDATION .. ":material", damage=23}
 ITEM_TIN_DUST = {name=MOD_THERMAL_FOUNDATION .. ":material", damage=65}
 ITEM_TIN_INGOT = {name=MOD_THERMAL_FOUNDATION .. ":material", damage=129}
 ITEM_TIN_PLATE = {name=MOD_THERMAL_FOUNDATION .. ":material", damage=321}
+ITEM_WOODEN_GEAR = {name=MOD_THERMAL_FOUNDATION .. ":material", damage=22}
 
 
 -- recipes
@@ -90,6 +105,7 @@ ITEM_TIN_PLATE = {name=MOD_THERMAL_FOUNDATION .. ":material", damage=321}
 RECIPE_DIAMOND_PICKAXE = {{"ddd", ".s", ".s"}, {d=ITEM_DIAMOND, s=ITEM_STICK}}
 RECIPE_FURNACE = {{"ccc", "c.c", "ccc"}, {c=ITEM_COBBLESTONE}}
 RECIPE_PLANKS = {{"w"}, {w=ITEM_LOG}}
+RECIPE_PISTON = {{"ppp", "sis", "srs"}, {p=ITEM_PLANKS, s=ITEM_COBBLESTONE, i=ITEM_IRON_INGOT, r=ITEM_REDSTONE}}
 RECIPE_STICK = {{"p", "p"}, {p=ITEM_PLANKS}}
 
 
@@ -100,11 +116,23 @@ RECIPE_IRON_GEAR = {{".i", "i.i", ".i"}, {i=ITEM_IRON_INGOT}}
 RECIPE_QUARRY = {{"iri", "gig", "dpd"}, {
   i=ITEM_IRON_GEAR, r=ITEM_REDSTONE, g=ITEM_GOLD_GEAR, d=ITEM_DIAMOND_GEAR, p=ITEM_DIAMOND_PICKAXE
 }}
+RECIPE_STIRLING_ENGINE = {{"sss", ".g", "GpG"}, {s=ITEM_STONE, g=ITEM_GLASS, G=ITEM_STONE_GEAR, p=ITEM_PISTON}}
+RECIPE_STONE_GEAR = {{".s", "sgs", ".s"}, {s=ITEM_STICK, g=ITEM_WOODEN_GEAR}}
+RECIPE_WOODEN_GEAR = {{".s", "s.s", ".s"}, {s=ITEM_STICK}}
+
+
+-- recipes: energy converters
+RECIPE_ENERGY_CONSUMER_EU_LV = {{"p"}, {p=ITEM_ENERGY_PRODUCER_EU_LV}}
+RECIPE_ENERGY_PRODUCER_EU_LV = {{"scs", "tmg", "sCs"}, {
+  s=ITEM_STONE, c=ITEM_INSULATED_TIN_CABLE, t=ITEM_LV_TRANSFORMER,
+  i=ITEM_BASIC_MACHINE_CASING, g=ITEM_GOLD_INGOT, C=ITEM_COIL
+}}
 
 
 -- recipes: industrial craft recipes
 --  note: cables cannot be automatically crafted at this time because of how getItemDetail() does not
 --    manage to return enough information about items
+RECIPE_BASIC_MACHINE_CASING = {{"ppp", "p.p", "ppp"}, {p=ITEM_IRON_PLATE}}
 RECIPE_BATBOX = {{"pcp", "bbb", "ppp"}, {p=ITEM_PLANKS, c=ITEM_INSULATED_TIN_CABLE, b=ITEM_BATTERY}}
 RECIPE_BATTERY = {{".w", "crc", "crc"}, {w=ITEM_INSULATED_TIN_CABLE, c=ITEM_TIN_CASING, r=ITEM_REDSTONE}}
 RECIPE_COPPER_CABLE = {{"cp"}, {c=ITEM_CUTTER, p=ITEM_COPPER_PLATE}}
@@ -136,15 +164,20 @@ RECIPES = {
   -- minecraft item recipes
   ITEM_DIAMOND_PICKAXE=RECIPE_DIAMOND_PICKAXE,
   ITEM_FURNACE=RECIPE_FURNACE,
+  ITEM_PISTON=RECIPE_PISTON,
   ITEM_PLANKS=RECIPE_PLANKS,
   ITEM_STICK=RECIPE_STICK,
 
   -- buildcraft item recipes
-  ITEM_DIAMOND_GEAR=RECIPE_DIAMOND_GEAR,
-  ITEM_GOLD_GEAR=RECIPE_GOLD_GEAR,
-  ITEM_IRON_GEAR=RECIPE_IRON_GEAR,
+  ITEM_QUARRY=RECIPE_QUARRY,
+  ITEM_STONE_GEAR=RECIPE_STONE_GEAR,
+  ITEM_WOODEN_GEAR=RECIPE_WOODEN_GEAR,
+
+  -- energy converter recipes
+  ITEM_ENERGY_CONSUMER_EU_LV=RECIPE_ENERGY_CONSUMER_EU_LV,
 
   -- industrial craft item recipes
+  ITEM_BASIC_MACHINE_CASING=RECIPE_BASIC_MACHINE_CASING,
   ITEM_BATBOX=RECIPE_BATBOX,
   ITEM_BATTERY=RECIPE_BATTERY,
   ITEM_CIRCUIT=RECIPE_CIRCUIT,
@@ -159,6 +192,9 @@ RECIPES = {
 
   -- thermal foundation
   ITEM_COPPER_PLATE=RECIPE_COPPER_PLATE,
+  ITEM_DIAMOND_GEAR=RECIPE_DIAMOND_GEAR,
+  ITEM_GOLD_GEAR=RECIPE_GOLD_GEAR,
+  ITEM_IRON_GEAR=RECIPE_IRON_GEAR,
   ITEM_IRON_PLATE=RECIPE_IRON_PLATE,
   ITEM_TIN_CASING=RECIPE_TIN_CASING,
   ITEM_TIN_PLATE=RECIPE_TIN_PLATE
